4ebbf75ca70275c192805c50a0013fc51e43b60a
jaideep777 <jaideep777@gmail.com>
2022-02-18 18:35:00 +0100

    rename docs folder to avoid conflicts with pkgdown

--- build_docs.sh
+++ build_docs.sh
@@ -1,6 +1,2 @@
-mv docs/Doxyfile Doxyfile
-mv docs/doxygen-awesome.css doxygen-awesome.css
 R -e "Sys.setenv(RSTUDIO_PANDOC='/usr/lib/rstudio/bin/pandoc'); pkgdown::clean_site(); pkgdown::build_site()"
-mv Doxyfile docs/Doxyfile 
-mv doxygen-awesome.css docs/doxygen-awesome.css 
-doxygen docs/Doxyfile
+doxygen doxygen/Doxyfile
